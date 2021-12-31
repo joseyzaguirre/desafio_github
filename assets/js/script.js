f7eb11940f1a64d8a76c5cac3a5461944cbbc503
joseyzaguirre <josemanuel.eyzaguirre@gmail.com>
2021-12-30 22:30:28 -0300

    desafío finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,24 +11,36 @@ $("form").submit(function(ev){
 
 async function getUser(username) {
     usuario_github = await request(username);
+    
+    $("#datosUser").html(" ");
 
-    $("#resultados").html(`
-    <div class="col-6 text-left">
-        <h1>Dato del Usuario</h1>
-        <img src="${usuario_github.avatar_url}" alt="avatar" class="img-fluid w-50">
-        <p>Nombre de usuario: ${usuario_github.name}</p>
-        <p>Nombre de login: ${usuario_github.login}</p>
-        <p>Cantidad de repositorios: ${usuario_github.public_repos}</p>
-        <p>Localidad: ${usuario_github.location}</p>
-        <p>Tipo de usuario: ${usuario_github.type}</p>
-    </div>
-    `)
+    $("#datosUser").append(`
+      <h1>Dato del Usuario</h1>
+      <img src="${usuario_github.avatar_url}" alt="avatar" class="img-fluid w-50">
+      <p>Nombre de usuario: ${usuario_github.name}</p>
+      <p>Nombre de login: ${usuario_github.login}</p>
+      <p>Cantidad de repositorios: ${usuario_github.public_repos}</p>
+      <p>Localidad: ${usuario_github.location}</p>
+      <p>Tipo de usuario: ${usuario_github.type}</p>
+    `);
 
 };
 
 async function getRepo(username, page, perpage) {
     const repos = await request(`${username}/repos?page=${page}&per_page=${perpage}`);
     console.log(repos)
+
+    $("#repos").html(" ")
+
+    $("#repos").append(`
+        <h1>Nombre de Repositorios</h1>
+    `);
+
+    for (repo of repos){
+    $("#repos").append(`
+        <p><a href="${repo.html_url}" target="_blank">${repo.name}</a></p> 
+    `);
+    };
 }
 
 async function request(path) {
